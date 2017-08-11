baa0bbbc9169bbfaec715f8978b7731a41e963cc
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-08-11 13:23:57 +0200

    Removed mongo from the stack.
    This is now completely run by node, and reads iso standards from file structure, which makes for simpler, cleaner and less complex code.

--- server.js
+++ server.js
@@ -1,93 +1,53 @@
+const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
-const app = express();
 
-const config = require('./lib/config');
-const isoService = require('./lib/isoService');
-const importService = require('./lib/mongoImportService');
+// Read configuration
+const config = {
+    WEB_SERVER_PORT: process.env.WEB_SERVER_PORT || 8000,
+    BASE_URL       : process.env.BASE_URL || '/felles/kodeverk'
+}
+const isoMapper = {
+    '31661alpha2': 'iso_3166_1_alfa_2',
+    '6391alpha2' : 'iso_639_1_alfa_2',
+    '5218'       : 'iso_5218'
+}
 
-app.use(cors());
+// Create express application
+express()
+    .use(cors())
 
-app.get(config.ISO_BASE_URL + '/31661alpha2', function(request, response) {
-    try {
-        console.log('Getting ISO 3166-1 alpha2 list');
-        isoService.getISO31661alpha2List(function(codes) {
-            response.send(codes);
-        });
-    } catch (ex) {
-        response.status(500).send(JSON.stringify(ex));
-    }
-});
+    // Setup endpoints
+    .get(`${config.BASE_URL}/:type/:iso`, (req, res) => res.send(getAll(req.params.type, req.params.iso)))
+    .get(`${config.BASE_URL}/:type/:iso/system/:id`, (req, res) => {
+        const json = getOne(req.params.type, req.params.iso, req.params.id);
+        return (json ? res.status(200).send(json) : res.status(404).send());
+    })
 
-app.get(config.ISO_BASE_URL + '/31661alpha2/systemid/:id', function(request, response) {
-    try {
-        console.log('Getting ISO 3166-1 alpha2 code');
-        isoService.getISO31661alpha2Code(request.params.id, function(codes) {
-            if (codes.length == 1) {
-                response.send(codes[0]);
-            } else {
-                response.status(404).send();
-            }
-        });
-    } catch (ex) {
-        response.status(500).send(JSON.stringify(ex));
-    }
-});
+    // Start server
+    .listen(config.WEB_SERVER_PORT, function() {
+        console.log('Started on port ' + config.WEB_SERVER_PORT);
+    });
 
-app.get(config.ISO_BASE_URL + '/6391alpha2', function(request, response) {
-    try {
-        console.log('Getting ISO 639-1 alpha2 list');
-        isoService.getISO6391alpha2List(function(codes) {
-            response.send(codes);
-        });
-    } catch (ex) {
-        response.status(500).send(JSON.stringify(ex));
-    }
-});
 
-app.get(config.ISO_BASE_URL + '/6391alpha2/systemid/:id', function(request, response) {
-    try {
-        console.log('Getting ISO 639-1 alpha2 code');
-        isoService.getISO6391alpha2Code(request.params.id, function(codes) {
-            if (codes.length == 1) {
-                response.send(codes[0]);
-            } else {
-                response.status(404).send();
-            }
-        });
-    } catch (ex) {
-        response.status(500).send(JSON.stringify(ex));
-    }
-});
+/**
+ * Get the entire data from the iso standard
+ * 
+ * @param {string} type The iso category to fetch
+ * @param {string} iso The name of the iso standard to fetch
+ */
+function getAll(type, iso) {
+    const standard = isoMapper[iso];
+    return fs.readFileSync(`${__dirname}/kodeverk/${type}/${standard}.json`, 'utf-8');
+}
 
-app.get(config.ISO_BASE_URL + '/5218', function(request, response) {
-    try {
-        console.log('Getting ISO 5218 list');
-        isoService.getISO5218List(function(codes) {
-            response.send(codes);
-        });
-    } catch (ex) {
-        response.status(500).send(JSON.stringify(ex));
-    }
-});
-
-app.get(config.ISO_BASE_URL + '/5218/systemid/:id', function(request, response) {
-    try {
-        console.log('Getting ISO 5218 code');
-        isoService.getISO5218Code(request.params.id, function(codes) {
-            if (codes.length == 1) {
-                response.send(codes[0]);
-            } else {
-                response.status(404).send();
-            }
-        });
-    } catch (ex) {
-        response.status(500).send(JSON.stringify(ex));
-    }
-});
-
-importService.importISOCodeLists();
-
-app.listen(config.WEB_SERVER_PORT, function() {
-    console.log('Started on port ' + config.WEB_SERVER_PORT);
-});
+/**
+ * Get one entry in the iso standard
+ * 
+ * @param {string} type The iso category to fetch
+ * @param {string} iso The name of the iso standard to fetch
+ * @param {string} id  The systemId of the entry in the standard
+ */
+function getOne(type, iso, id) {
+    return JSON.parse(getAll(type, iso)).find(m => m.systemId.identifikatorverdi === id);
+}
